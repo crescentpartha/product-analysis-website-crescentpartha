678da549eb5dee529e20d5bb82145a046545f12e
crescentpartha <crescentpartha406@gmail.com>
2022-08-03 20:40:18 +0600

    React FontAwesome Icons added

--- product-analysis-website/src/components/CustomerReview/CustomerReview.js
+++ product-analysis-website/src/components/CustomerReview/CustomerReview.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarDays, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 const CustomerReview = (props) => {
     const { name, picture, review, registered } = props.review;
     return (
         <div className='text-left shadow-inner rounded p-4'>
-            <div className='flex flex-row flex-warp items-center justify-start'>
-                <img className='w-16 rounded-full mr-3' src={picture} alt="" />
+            <div className='flex flex-row items-center justify-between'>
+                <div className='flex flex-row flex-warp items-center justify-start'>
+                    <img className='w-16 rounded-full mr-3' src={picture} alt="" />
+                    <div>
+                        <h2 className='text-lg font-semibold'>{name}</h2>
+                        <p className='text-sm font-normal'>
+                            <FontAwesomeIcon className='mr-1 text-orange-300' icon={faCalendarDays}></FontAwesomeIcon>
+                            {registered}
+                        </p>
+                    </div>
+                </div>
                 <div>
-                    <h2 className='text-lg font-semibold'>{name}</h2>
-                    <p className='text-sm font-normal'>Date: {registered}</p>
+                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-300' icon={faStarHalfStroke}></FontAwesomeIcon>
                 </div>
             </div>
             <p className='text-justify'>{(review.length > 200) ? review.slice(0, 200) + "..." : review}</p>
--- product-analysis-website/src/components/CustomerReviews/CustomerReviews.js
+++ product-analysis-website/src/components/CustomerReviews/CustomerReviews.js
@@ -13,7 +13,7 @@ const CustomerReviews = () => {
 
     return (
         <div>
-            <h2 className='text-2xl font-semibold'>Customer Reviews (3)</h2>
+            <h2 className='text-2xl font-semibold mt-20'>Customer Reviews (3)</h2>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-flow-row gap-8 m-10 p-10 shadow-lg rounded-2xl'>
                 {
                     reviews.map(review => <CustomerReview
