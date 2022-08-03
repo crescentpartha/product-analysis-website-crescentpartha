622e8a2a64787b9a680b2cc8659be97d67ff0860
crescentpartha <crescentpartha406@gmail.com>
2022-08-03 23:47:33 +0600

    HOME route shows only 3 Customer Reviews

--- product-analysis-website/src/components/CustomerReview/CustomerReview.js
+++ product-analysis-website/src/components/CustomerReview/CustomerReview.js
@@ -3,29 +3,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 const CustomerReview = (props) => {
+    // console.log(props.review);
     const { name, picture, review, registered } = props.review;
     return (
         <div className='text-left shadow-inner rounded p-4'>
-            <div className='flex flex-row items-center justify-between'>
+            <div className='flex flex-row flex-wrap items-center justify-between'>
                 <div className='flex flex-row flex-warp items-center justify-start'>
-                    <img className='w-16 rounded-full mr-3' src={picture} alt="" />
+                    <img className='w-10 lg:w-12 md:w-10 sm:w-10 rounded-full mr-3' src={picture} alt="" />
                     <div>
-                        <h2 className='text-lg font-semibold'>{name}</h2>
-                        <p className='text-sm font-normal'>
-                            <FontAwesomeIcon className='mr-1 text-orange-300' icon={faCalendarDays}></FontAwesomeIcon>
+                        <h2 className='text-lg font-semibold md:text-sm'>{name}</h2>
+                        <p className='text-sm font-normal text-xm'>
+                            <FontAwesomeIcon className='mr-1 text-orange-300 text-xm' icon={faCalendarDays}></FontAwesomeIcon>
                             {registered}
                         </p>
                     </div>
                 </div>
-                <div>
-                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
-                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
-                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
-                    <FontAwesomeIcon className='text-orange-400' icon={faStar}></FontAwesomeIcon>
-                    <FontAwesomeIcon className='text-orange-300' icon={faStarHalfStroke}></FontAwesomeIcon>
+                <div >
+                    <FontAwesomeIcon className='text-orange-400 text-sm' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400 text-sm' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400 text-sm' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-400 text-sm' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='text-orange-300 text-sm' icon={faStarHalfStroke}></FontAwesomeIcon>
                 </div>
             </div>
-            <p className='text-justify'>{(review.length > 200) ? review.slice(0, 200) + "..." : review}</p>
+            <p className='text-justify text-sm mt-2'>{(review.length > 200) ? review.slice(0, 200) + "..." : review}</p>
         </div>
     );
 };
--- product-analysis-website/src/components/CustomerReviews/CustomerReviews.js
+++ product-analysis-website/src/components/CustomerReviews/CustomerReviews.js
@@ -8,12 +8,23 @@ const CustomerReviews = () => {
     // const [reviews, setReviews] = useReviews();
     const [reviews] = useReviews();
 
+    let rest = [];
+    const handleCustomerReview = () => {
+        reviews.forEach(element => {
+            if (rest.length < 3) {
+                rest.push(element);
+            }
+        });
+    }
+    handleCustomerReview();
+    // console.log(rest);
+
     return (
         <div>
-            <h2 className='text-2xl font-semibold mt-20'>Customer Reviews ({reviews.length})</h2>
+            <h2 className='text-2xl font-semibold mt-10'>Customer Reviews ({rest.length})</h2>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-flow-row gap-8 m-10 p-10 shadow-lg rounded-2xl'>
                 {
-                    reviews.map(review => <CustomerReview
+                    rest.map(review => <CustomerReview
                         key={review.id}
                         review={review}
                     ></CustomerReview>)
