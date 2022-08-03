03eaf35c4966e792cc2ed707415b3cfbf819d2ab
crescentpartha <crescentpartha406@gmail.com>
2022-08-03 13:10:52 +0600

    Create all Header components likes Reviews, Dashboard, Blogs, About and Set the Not Found page

--- product-analysis-website/src/components/Header/Header.js
+++ product-analysis-website/src/components/Header/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
         <div>
             <h2>Welcome to my Product Analysis Website</h2>
             <Link to='/home'>Home</Link>
+            <Link to='/reviews'>Reviews</Link>
+            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/blogs'>Blogs</Link>
+            <Link to='/about'>About</Link>
         </div>
     );
 };
