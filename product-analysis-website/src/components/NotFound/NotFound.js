889fb77d248a122a9e7f66aad017ca054d81d40e
crescentpartha <crescentpartha406@gmail.com>
2022-08-04 15:02:26 +0600

    Not Found page is modified

--- product-analysis-website/src/components/NotFound/NotFound.js
+++ product-analysis-website/src/components/NotFound/NotFound.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='bg-red-200 my-20 p-20 font-semibold text-lg mx-20 rounded-xl'>
             <h2>The page is not Available!!!</h2>
-            <p>404</p>
+            <p className='text-orange-400 p-1'>404</p>
         </div>
     );
 };
